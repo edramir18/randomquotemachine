de376b758f89d19f8a9f8f142675f9252d82b2ed
Eduardo Ramirez <edramir18@gmail.com>
2018-01-08 14:13:35 -0400

    Add json query to genereate quotes

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function updateQuote(){
+	var url = "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=1";	
+	$.ajax({
+		url: url,
+		dataType: "json",
+		headers: {
+			'X-Mashape-Key':"gWyj47zjvgmshQ7uwtX2eDuR6WYnp1slPgqjsn3tyiVNre0riP",
+	        'Content-Type': 'application/json'
+	    },
+	    type: "GET",
+		crossDomain: true,
+		success: function (json) {
+			if(json.hasOwnProperty("quote")){				
+				$("#cita p:first-child").html(json.quote);
+				$("#autor").html(json.author);
+			}
+		}
+	});
+};
+$(document).ready(function() {	
+	updateQuote();
+	$("#next-btn").on("click", function(){
+		updateQuote();
+	});
+});
